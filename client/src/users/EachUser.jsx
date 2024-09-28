fcd096ac400a831497706cbb5171f07c4985af69
arthurbrit0 <arthurbrito@alu.ufc.br>
2024-09-28 00:47:54 -0300

    add: adicao de usuario na lista

--- client/src/users/EachUser.jsx
+++ client/src/users/EachUser.jsx
@@ -3,16 +3,16 @@ import {Link} from 'react-router-dom'
 
 export default function EachUser({user, fetchData}) {
   return (
-    <div className="bg-slate-100 rounded-lg mb-4 p-4 hover:border hover:border-purple-700 hover:scale-105 duration-300">
+    <div className="bg-slate-100 rounded-lg mb-4 p-4 border-2 border-transparent hover:border-2 hover:border-purple-700 hover:scale-105 duration-300">
       <div>
         <div>
             <div className="font-medium">{user.nome}</div>
             <div className="text-slate-400">{user.email}</div>
         </div>
         <div className="text-sm flex space-x-4 mt-4">
-            <Link to={`/profile/${user.ID}`}>Ver perfil</Link>
-            <button>Editar</button>
-            <button className="text-red-600">Deletar</button>
+            <Link to={`/profile/${user.ID}`} className="hover:scale-110 transition-all transition-duration-3">Ver perfil</Link>
+            <button className="hover:scale-110 transition-all">Editar</button>
+            <button className="text-red-600 hover:scale-110 transition-all">Deletar</button>
         </div>
       </div>
     </div>
--- client/src/users/List.jsx
+++ client/src/users/List.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { Link, useSearchParams, useNavigate } from 'react-router-dom'
 import { API_URL } from '../config'
 import EachUser from './EachUser';
+import axios from 'axios';
 
 export default function List() {
 
@@ -29,6 +30,30 @@ export default function List() {
         fetchData()
     }, [searchParams])
 
+    const openModal = () => {
+        document.getElementById("new-modal").classList.remove("hidden")
+    }
+
+    const closeModal = () => {
+        document.getElementById("new-modal").classList.add("hidden")
+    }
+
+    const completeForm = (form) => {
+        closeModal()
+        form.reset()
+        fetchData()
+        navigate('/')
+    }
+
+    const storeUser = async (e) => {
+        e.preventDefault()
+        var form = document.getElementById('newform')
+        var formData = new FormData(form)
+        axios.post(`${API_URL}/users`, formData)
+        .then(res => completeForm(form))
+        .catch(error => console.log(error.response))
+    }
+
     let myPage = searchParams.get("page") ? searchParams.get("page") : 0
 
     return (
@@ -37,7 +62,7 @@ export default function List() {
                 <div className="p-10">
                     <div className="mb-10 flex items-center justify-between">
                         <h1 className="font-bold">CRUD em Golang + React</h1>
-                        <button className="bg-purple-700 text-white px-3 py-2 rounded hover:scale-105 duration-300">Adicionar usuário</button>
+                        <button className="bg-purple-700 text-white px-3 py-2 rounded hover:scale-105 duration-300" onClick={openModal}>Adicionar usuário</button>
                     </div>
                     <div>
                         {users.length > 0 ? users.map((user, key) => <EachUser key={key} user={user} fetchData={fetchData}/>) : ''} 
@@ -45,8 +70,53 @@ export default function List() {
 
                     <div className="mt-10">
                             {Array.from({ length: pages }, (_, index) => index + 1).map((pg, key) =>
-                                <Link className={`border px-3 py-1 mr-3 rounded ${myPage == key ? 'bg-purple-600 text-purple-100' : ''}`} to={`?page=${key}`} key={key}>{key + 1}</Link>)}
+                                <Link className={`border px-3 py-1 mr-3 rounded hover:scale-105 duration-300 ${myPage == key ? 'hover:scale-105 bg-purple-600 text-purple-100' : ''}`} to={`?page=${key}`} key={key}>{key + 1}</Link>)}
+                    </div>
+
+                    <div className="relative z-10 hidden" aria-labelledby='modal-title' role="dialog" aria-modal="true" id="new-modal">
+                        <div className="fixed inset-0 bg-black bg-opacity-70 transition-opacity"></div>
+                        <div className="fixed z-10 inset-0 overflow-y-auto">
+                            <div className="flex items-end justify-center min-h-screen pt-4 pb-20 text-center sm:block sm:p-0">
+                                    <span className="hidden sm:inline sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                                    <div className="relative inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg w-full">
+                                        <form id="newform" onSubmit={storeUser} action="">
+                                            <div className="bg-white">
+                                                <div className="flex justify-between px-8 py-4 border-b">
+                                                    <h1 className="font-medium">Criar novo usuário</h1>
+                                                    <button type="button" onClick={closeModal}>Fechar</button>
+                                                </div>
+                                                <div className="px-8 py-8">
+                                                    <div className="mb-5">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2">Nome</label>
+                                                        <input type="text" name="nome" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required/>
+                                                    </div>
+                                                    <div className="mb-5">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                                                        <input type="text" name="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required/>
+                                                    </div>
+                                                    <div className="mb-5">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2">Data</label>
+                                                        <input type="text" name="data" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required/>
+                                                    </div>
+                                                    <div className="mb-5">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2">Cidade</label>
+                                                        <input type="text" name="cidade" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required/>
+                                                    </div>
+                                                    <div className="mb-5">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2">País</label>
+                                                        <input type="text" name="pais" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required/>
+                                                    </div>
+                                                    <div className="flex justify-end">
+                                                        <button type="submit" className="bg-blue-500 text-white py-1.5 px-4 rounded hover:scale-105 transition-all">Adicionar</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                            </div>
                         </div>
+                    </div>
+
                 </div>
             </div>
         </div>
